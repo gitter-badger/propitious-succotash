fc0c38df027bdf1c76244b3eeef6f9fb643d7769
fallen <outputexception@gmail.com>
2015-10-15 00:10:22 +0200

    a teraz dodałem projekcik w javce :)

---
+++ src/pl/pietras/tomasz/Main.java
@@ -0,0 +1,11 @@
+package pl.pietras.tomasz;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello GitHUB");
+	}
+
+}
